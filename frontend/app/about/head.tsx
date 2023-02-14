1bdc66ff8ec4fe2fa12315e3d90089e6e72fcbea
JingusJohn <branchjohnchristopher@gmail.com>
2023-02-13 23:54:03 -0600

    removed some boilerplate and added some examples

---
+++ frontend/app/about/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>About Page</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Brief description of this website's purpose" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
